a913ec9d3c7ea40abfeb7130dd1103fdbc33dc14
duleigiser <564779666@qq.com>
2020-09-22 21:04:46 +0800

    bug

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,7 @@
 const path = require("path")
 const uploadFiles = require("../upload")
 const version = require('../package.json').version
-var conf = require(path.join(__dirname, 'uploadConf.json'))
+var conf = require(path.join(process.cwd(), 'uploadConf.json'))
 if(!conf) {
   console.err('请在项目根目录配置uploadConf')
   return
