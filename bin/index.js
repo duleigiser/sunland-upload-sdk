d092a326dedf53597d4757192b003caf4ef3e5ab
duleigiser <564779666@qq.com>
2020-09-22 20:52:03 +0800

    bugfixed

--- bin/index.js
+++ bin/index.js
@@ -3,11 +3,11 @@
 const path = require("path")
 const uploadFiles = require("../upload")
 const version = require('../package.json').version
-var conf = require(path.join(process.cwd(), 'uploadConf.json'))
+var conf = require(path.join(__dirname, 'uploadConf.json'))
 if(!conf) {
   console.err('请在项目根目录配置uploadConf')
   return
 } 
 console.log(`---------- verson ${version}-------`)
-console.log(`dist ====> ${conf.remotePath}, please wait...`)
+console.log(`${conf.localPath} ====> ${conf.remotePath}, please wait...`)
 uploadFiles(conf)
