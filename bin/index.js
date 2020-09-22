6158f42260a3ad0d0220c1c041c71d803fd71377
duleigiser <564779666@qq.com>
2020-09-22 20:39:22 +0800

    调整位置

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 const path = require("path")
-const uploadFiles = require("./bin/upload")
-const version = require('./package.json').version
+const uploadFiles = require("../upload")
+const version = require('../package.json').version
 var conf = require(path.join(process.cwd(), 'uploadConf.json'))
 if(!conf) {
   console.err('请在项目根目录配置uploadConf')
