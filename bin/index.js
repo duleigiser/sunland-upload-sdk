2dd343816ebf9aadfd1c2ce5c62511de4f7131af
duleigiser <564779666@qq.com>
2020-09-23 11:12:58 +0800

    删除无关代码

--- bin/index.js
+++ bin/index.js
@@ -4,10 +4,7 @@ const path = require("path")
 const uploadFiles = require("../upload")
 const version = require('../package.json').version
 var conf = require(path.join(process.cwd(), 'uploadConf.json'))
-if(!conf) {
-  console.err('请在项目根目录配置uploadConf')
-  return
-} 
-console.log(`---------- verson ${version}-------`)
+
+console.log(`---------- version ${version}-------`)
 console.log(`${conf.localPath} ====> ${conf.remotePath}, please wait...`)
 uploadFiles(conf)
